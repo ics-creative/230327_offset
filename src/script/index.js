a20c8e9242858e821ba853ed96d9377fce2e3428
ics-sawada-h <haruka.sawada@ics-web.jp>
2023-03-17 13:41:12 +0900

    first commit

---
+++ src/script/index.js
@@ -0,0 +1,56 @@
+/**
+ * ボタン押下でアニメーション再生する用のjsファイルです
+ * */
+window.addEventListener("DOMContentLoaded", () => {
+  init();
+})
+
+const init = () => {
+  clickAndPlay("faces", "faces", 4000, false);
+  clickAndPlay("letters", "letters", 5500, true);
+}
+
+/** アニメーションを再生させるために付与するクラス名 */
+const ACTIVE_CLASS_NAME = "is-active"
+
+/**
+ * ボタン押下でアニメーションを再生するためのactiveクラスを付与します
+ * @param buttonId {string} data-button-id属性に指定した値
+ * @param wrapperId {string} data-wrapper-id属性に指定した値
+ * @param animDuration {number} CSSのtransitionプロパティに指定したものと同じduration
+ * @param shouldToggle {boolean} activeクラスの付与を切り替えるか？ アニメーション再生後にそのままにしたい場合はtrue
+ * */
+const clickAndPlay = (buttonId, wrapperId, animDuration, shouldToggle) => {
+  const button = document.querySelector(`[data-button-id="${buttonId}"]`);
+  const wrapper = document.querySelector(`[data-wrapper-id="${wrapperId}"]`);
+  /** 再生中か？ */
+  let isPlaying = false;
+  /** タイマー */
+  let timeoutID = null;
+
+  button.addEventListener("click", (event) => {
+    if (isPlaying) {
+      event.preventDefault();
+      return;
+    }
+
+    if (wrapper.classList.contains(ACTIVE_CLASS_NAME)) {
+      button.innerHTML = "再生する"
+      wrapper.classList.remove(ACTIVE_CLASS_NAME);
+      clearTimeout(timeoutID);
+    } else {
+      wrapper.classList.add(ACTIVE_CLASS_NAME);
+      isPlaying = true;
+      timeoutID = setTimeout(() => {
+        isPlaying = false;
+
+        if (!shouldToggle) {
+          wrapper.classList.remove(ACTIVE_CLASS_NAME);
+        } else {
+          button.innerHTML = "リセット";
+        }
+      }, animDuration)
+    }
+  });
+}
+
